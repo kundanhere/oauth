b0da9b4d8e5ba76bd773ac5753f44895ec49de6b
Kundan Gupta <kundankumar8377@gmail.com>
2020-12-19 09:53:00 +0530

    passport initial setup

---
+++ config/passport-setup.js
@@ -0,0 +1,12 @@
+const { Strategy } = require('passport');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+
+passport.use(
+  new GoogleStrategy({
+    // options for google strategy
+  }),
+  () => {
+    // passport callback function
+  }
+);
